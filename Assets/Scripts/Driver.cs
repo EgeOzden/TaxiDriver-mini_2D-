947840e40c4a4f2a91dd3d2a11f82708209416dc
aegean1-admin <ege.00@hotmail.com>
2024-08-18 21:45:22 +0300

    Revert "Revert "v.032 layout update""
    This reverts commit ea75f8cfca036d990284ce5d23099b9aa7cf3291.

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -6,7 +6,7 @@ public class Driver : MonoBehaviour
 {
 
     [SerializeField] float turnSpeed = 90f;
-    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] float moveSpeed = 8f;
     void Start()
     {
 
