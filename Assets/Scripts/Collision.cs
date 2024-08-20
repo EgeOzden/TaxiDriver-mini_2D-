34a1d7c763a1cea0846a0c72ded106d1bfbd3d86
aegean1-admin <ege.00@hotmail.com>
2024-08-21 01:44:14 +0300

    v0.9 update

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -6,12 +6,22 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+
+    float delay = 3f;
     bool statu;
+    [SerializeField] Color32 statu_color;
+    SpriteRenderer sprite_renderer;
+    [SerializeField] public GameObject driverObject; //Driver scriptinin içinde bulunduğu oyun objesi inspectordan referans verilir. public olmalı
+    
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Başka bir obje ile temas edildi.");
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
+
+        Driver boost_speed = driverObject.GetComponent<Driver>(); //part1
+
+
         if (other.tag == "customer" && statu == false) {
             Debug.Log("Müşteri alındı.");
             statu = true;
@@ -20,7 +30,26 @@ public class Collision : MonoBehaviour
 
         if (other.tag == "stop" && statu == true) {
             Debug.Log("Müşteri hedefe ulaştırıldı.");
+            sprite_renderer = other.GetComponent<SpriteRenderer>();
+            sprite_renderer.color = statu_color;
             statu = false;
         }
+
+        if (other.tag == "boost") { // part2 driver scripttindeki moveSpeed değişkenine erişim, not: moveSpeed değişkeni public olmalı!
+            boost_speed.moveSpeed *= 1.4f;
+            Destroy(other.gameObject, 0.15f);
+            Invoke("TurnNormalSpeed", delay);
+        }
+
+        if (other.tag == "debuff"){
+            boost_speed.moveSpeed = 4f;
+            Destroy(other.gameObject, 0.15f);
+            Invoke("TurnNormalSpeed", delay); //delaylı olarak methodu tetikleyip hız eski haline getirilir.
+        }
+    }
+
+    public void TurnNormalSpeed() {
+        Driver boost_speed = driverObject.GetComponent<Driver>(); 
+        boost_speed.moveSpeed = 8f;
     }
 }
--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -6,7 +6,7 @@ public class Driver : MonoBehaviour
 {
 
     [SerializeField] float turnSpeed = 110f;
-    [SerializeField] float moveSpeed = 8f;
+    [SerializeField] public float moveSpeed = 8f;
     void Start()
     {
 
